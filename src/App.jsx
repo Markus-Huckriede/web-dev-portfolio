09ac1ba46dc7c0f1cb8f3de4152ad64094122a7a
markushuckriede <Markus.Huckriede@gmail.com>
2026-02-05 15:19:31 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hallo Welt!</h1>
+        <p>Web Development</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
